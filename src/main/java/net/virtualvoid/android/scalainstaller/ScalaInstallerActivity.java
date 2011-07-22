e59ab317cd11d3ee55816d23aa996cc915bdea3e
Johannes Rudolph <johannes_rudolph@gmx.de>
2011-07-23 00:03:03 +0200

    some errm... screen design and descriptive text

--- src/main/java/net/virtualvoid/android/scalainstaller/ScalaInstallerActivity.java
+++ src/main/java/net/virtualvoid/android/scalainstaller/ScalaInstallerActivity.java
@@ -2,6 +2,8 @@ package net.virtualvoid.android.scalainstaller;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class ScalaInstallerActivity extends Activity {
@@ -9,12 +11,22 @@ public class ScalaInstallerActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        ScalaInstaller installer = new ScalaInstaller(getApplicationContext());
-        installer.installScalaLibs();
+        setContentView(R.layout.main);
 
-        TextView text = new TextView(this);
-        text.setText("Everything worked well");
-        setContentView(text);
+        final TextView text = (TextView) findViewById(R.id.text);
+
+        final Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                ScalaInstaller installer = new ScalaInstaller(getApplicationContext());
+                installer.installScalaLibs();
+                button.setEnabled(false);
+
+                text.setTextSize(20);
+                text.setText("Finished! Please restart your phone.");
+            }
+        });
     }
 }
 
