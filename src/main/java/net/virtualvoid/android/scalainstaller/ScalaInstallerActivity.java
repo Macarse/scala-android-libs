8d36cff3ec9a0830cdb045fbf8f0d47f930fafc5
Johannes Rudolph <johannes_rudolph@gmx.de>
2011-07-23 00:15:14 +0200

    show a progress dialog while running

--- src/main/java/net/virtualvoid/android/scalainstaller/ScalaInstallerActivity.java
+++ src/main/java/net/virtualvoid/android/scalainstaller/ScalaInstallerActivity.java
@@ -1,30 +1,50 @@
 package net.virtualvoid.android.scalainstaller;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class ScalaInstallerActivity extends Activity {
+public class ScalaInstallerActivity extends Activity implements Runnable {
+    ProgressDialog progress;
+    TextView text;
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.main);
 
-        final TextView text = (TextView) findViewById(R.id.text);
+        text = (TextView) findViewById(R.id.text);
 
-        final Button button = (Button) findViewById(R.id.button);
+        button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new Button.OnClickListener(){
             @Override
             public void onClick(View view) {
-                ScalaInstaller installer = new ScalaInstaller(getApplicationContext());
-                installer.installScalaLibs();
+                progress = ProgressDialog.show(ScalaInstallerActivity.this, "Installing...", "");
+
+                Thread thread = new Thread(ScalaInstallerActivity.this);
+                thread.start();
+            }
+        });
+    }
+
+    @Override
+    public void run() {
+        ScalaInstaller installer = new ScalaInstaller(getApplicationContext());
+        installer.installScalaLibs();
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
                 button.setEnabled(false);
 
                 text.setTextSize(20);
                 text.setText("Finished! Please restart your phone.");
+
+                progress.dismiss();
             }
         });
     }
